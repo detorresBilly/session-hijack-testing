7ab4a8a35ac166e9990059b5afe6aac436f26904
colll78 <philipdisarro@gmail.com>
2023-10-12 14:12:05 -0700

    Update user.js
    Add comments and ASCII art

--- public/user.js
+++ public/user.js
@@ -1,7 +1,43 @@
+//document event listener
+//                      ___..-.---.---.--..___
+//                _..-- `.`.   `.  `.  `.      --.._
+//               /    ___________\   \   \______    \
+//               |   |.-----------`.  `.  `.---.|   |
+//               |`. |'  \`.        \   \   \  '|   |
+//               |`. |'   \ `-._     `.  `.  `.'|   |
+//              /|   |'    `-._o)\  /(o\   \   \|   |\
+//            .' |   |'  `.     .'  '.  `.  `.  `.  | `.
+//           /  .|   |'    `.  (_.==._)   \   \   \ |.  \         _.--.
+//         .' .' |   |'      _.-======-._  `.  `.  `. `. `.    _.-_.-'\\
+//        /  /   |   |'    .'   |_||_|   `.  \   \   \  \  \ .'_.'     ||
+//       / .'    |`. |'   /_.-'========`-._\  `.  `-._`._`. \(.__      :|
+//      ( '      |`. |'.______________________.'\      _.) ` )`-._`-._/ /
+//       \\      |   '.------------------------.'`-._-'    //     `-._.'
+//       _\\_    \    | AMIGA  O O O O * * `.`.|    '     //
+//      (_  _)    '-._|________________________|_.-'|   _//_
+//      /  /      /`-._      |`-._     / /      /   |  (_  _)
+//    .'   \     |`-._ `-._   `-._`-._/ /      /    |    \  \
+//   /      `.   |    `-._ `-._   `-._|/      /     |    /   `.
+//  /  / / /. )  |  `-._  `-._ `-._          /     /   .'      \
+// | | | \ \|/   |  `-._`-._  `-._ `-._     /     /.  ( .\ \ \  \
+//  \ \ \ \/     |  `-._`-._`-._  `-._ `-._/     /  \  \|/ / | | |
+//   `.\_\/       `-._  `-._`-._`-._  `-._/|    /|   \   \/ / / /
+//               /    `-._  `-._`-._`-._  ||   / |    \   \/_/.'
+//             .'         `-._  `-._`-._  ||  /  |     \
+//    LGB     /           / . `-._  `-._  || /   |      \
+//           '\          / /      `-._    ||/'._.'       \
+//            \`.      .' /           `-._|/              \
+//             `.`-._.' .'               \               .'
+//               `-.__\/                 `\            .' '
+//                                        \`.       _.' .'
+//                                         `.`-._.-' _.'
+//                                           `-.__.-'
+
 document.addEventListener('DOMContentLoaded', () => {
+    //use a js framework 
     const dataList = document.getElementById('post-list');
     const userHeader = document.getElementById('user-header')
-
+    // async api call to user endpoint
     fetch('/api/user')
     .then((response) => response.json())
     .then((data) => {
@@ -10,7 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
     .catch((error) => {
       console.error('Error fetching data:', error);
     });
-  
+    // async api call to posts endpoint
     fetch('api/posts')
       .then((response) => response.json())
       .then((data) => {
@@ -37,5 +73,4 @@ document.addEventListener('DOMContentLoaded', () => {
       .catch((error) => {
         console.error('Error fetching data:', error);
       });
-
 });
